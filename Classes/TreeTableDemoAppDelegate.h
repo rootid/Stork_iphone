37cf2904872fe334a3967fecfc41a097066fb06a
rootkarma <vsinhsawant@gmail.com>
2012-08-17 18:45:08 -0300

    Update Classes/TreeTableDemoAppDelegate.h
    update Rest

--- Classes/TreeTableDemoAppDelegate.h
+++ Classes/TreeTableDemoAppDelegate.h
@@ -1,9 +1,6 @@
 //
 //  TreeTableDemoAppDelegate.h
-//  TreeTableDemo
 //
-//  Created by simone on 9/25/09.
-//  Copyright Simone Ardissone 2009. All rights reserved.
 //
 
 @interface TreeTableDemoAppDelegate : NSObject <UIApplicationDelegate> {
